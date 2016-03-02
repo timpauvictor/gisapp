0b7aef03ef42c8893ca7b613a3561dbfc4f72db0
Victor Timpau <timpauvictor@gmail.com>
2016-03-02 15:59:56 -0500

    some changes

--- js/mapScript.js
+++ js/mapScript.js
@@ -3,7 +3,7 @@ var __zoomLevel = 12;
 var __map;
 var __shapeLayers = [];
 var __activatedLayers = [];
-var __customMarkers = {};
+var __customMarkers = []; //0 is always our current location as long as we have it
 
 function loadMap() {
 	__map = L.map("map").setView(__startingCoords, __zoomLevel);
@@ -12,11 +12,12 @@ function loadMap() {
 	console.log('topographic layer added');
 }
 
-function addShapeFile(path) { //generic function to add shapeFiles to the map, then stores the added shpfile object in an array
+function addPolygonShapeFile(path) { //generic function to add shapeFiles to the map, then stores the added shpfile object in an array
 	console.log("attempting to load shapefile from " + path);
 	var shpfile = new L.Shapefile(path, {
 		onEachFeature: function(feature, layer) {
 			if (feature.properties) {
+				console.log(feature);
 				layer.bindPopup(Object.keys(feature.properties).map(function(k) {
 						return k + ": " + feature.properties[k];
 					}).join("<br />"), {
@@ -31,10 +32,41 @@ function addShapeFile(path) { //generic function to add shapeFiles to the map, t
 								// the tiles don't load properly
 	});
 	__shapeLayers.push(shpfile);
+	// console.log(shpfile.attribute('alt'));
 	__activatedLayers.push(false);
 }
 
+function addMarkerShapeFile(path) { //generic function to add shapeFiles to the map, then stores the added shpfile object in an array
+	console.log("attempting to load shapefile from " + path);
+	var shpfile = new L.Shapefile(path, {
+		onEachFeature: function(feature, layer) {
+			if (feature.properties) {
+				console.log(feature);
+				layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+						return k + ": " + feature.properties[k];
+					}).join("<br />"), {
+						maxHeight: 200
+					});
+			}
+		}
+	});
+	shpfile.once("data:loaded", function() {
+		console.log("finished loading from " + path);
+		__map.invalidateSize(); //leaflet has a bug where you either invalidate the size or
+								// the tiles don't load properly
+	});
+	__shapeLayers.push(shpfile);
+	// console.log(shpfile.attribute('alt'));
+	__activatedLayers.push(false);
+}
+
+
 function toggleLayer(index) {
+	if (__customMarkers[0] != undefined) {
+		__map.removeLayer(__customMarkers[0]);
+	} else {
+		//do nothing
+	}
 	if (__activatedLayers[index] === false) {
 		__shapeLayers[index].addTo(__map);
 		__activatedLayers[index] = true;
@@ -61,28 +93,44 @@ function findMe() {
 
 function showPosition(position) {
 	if (__customMarkers["geolocation"] != undefined) {
-		__map.removeLayer(__customMarkers["geolocation"]);
+		__map.removeLayer(__customMarkers[0]);
 	}
 	var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(__map);
-	__customMarkers["geolocation"] = marker;
+	__customMarkers[0] = marker;
 	__map.setView([position.coords.latitude, position.coords.longitude], 18);	
 }
 
 function addFindMeButton() {
 	L.easyButton( 'icon ion-android-locate larger',function() {
 		findMe();
-	}).addTo(__map);
+	},
+	"Find my location").addTo(__map);
 }
 
+function addMunicipalRecyclingButton() {
+	L.easyButton('icon ion-refresh larger', function() {
+		toggleLayer(2);
+	},
+	"Display municipal recycling locations").addTo(__map);
+}
 
-function mainRunner() {
+function addPrivateRecyclingButton() {
+	L.easyButton('icon ion-loop larger', function() {
+		toggleLayer(3);
+	},
+	"Display private recycling locations").addTo(__map);
+}
+
+function mapSetup() {
 	loadMap(); //loads map and adds it to div
-	addShapeFile("../data/wasteday.zip");
-	addShapeFile("../data/LeafYardServices.zip");
-	addShapeFile("../data/Recycling.zip");
-	// setTimeout({ __map.invalidateSize() }, 500);
+	addPolygonShapeFile("../data/wasteday.zip");
+	addPolygonShapeFile("../data/LeafYardServices.zip");
+	addMarkerShapeFile("../data/municipal.zip");
+	addMarkerShapeFile("../data/private.zip");
 	addFindMeButton();
+	addPrivateRecyclingButton();
+	addMunicipalRecyclingButton();
 
 }
 
-mainRunner();
+mapSetup();
